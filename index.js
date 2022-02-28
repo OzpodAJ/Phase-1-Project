6d0f607cb1262a64fc7ed8d9644caa4461272633
Andrew Osborn <osbornandrewj@gmail.com>
2022-02-28 14:45:16 -0800

    scaled back for simplicity

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+// var post= document.getElementById("post");
+// post.addEventListener("click", function(){
+//     var commentBoxValue= document.getElementById("comment-box").value;
+//     var li = document.createElement("li");
+//     var text = document.createTextNode(commentBoxValue);
+//     li.appendChild(text);
+//     document.getElementById("unordered").appendChild(li);
+ 
+// });
 window.addEventListener('load', (event) => {
-    let alpha = document.getElementById('enter');
-    let bravo = document.getElementById('textarea')
-    let charlie = document.getElementById('Username')
-    let delta = document.getElementById('color-picker')
-    console.log(alpha, bravo,'the page has loaded')
-    alpha.addEventListener('click', () => {
-        console.log(`${charlie.value}(${delta.value}) says ${bravo.value}`)
+    let post = document.getElementById('enter');
+    post.addEventListener('click', function(){
+        let comment = document.getElementById('textarea').value;
+        let p = document.createElement('p');
+        p.classList.add('message')
+        let text = document.createTextNode(comment)
+        p.appendChild(text);
+        document.getElementById('messages').appendChild(p)
     })
 })
