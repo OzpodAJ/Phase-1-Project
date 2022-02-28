a196c22a10e4c4ee2613855355ac8ba70d324c1f
Andrew Osborn <osbornandrewj@gmail.com>
2022-02-28 13:29:46 -0800

    another update

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const button = document.getElementById('enter');
-function clickButton(){ 
-    console.log('I was clicked')
-};
-button.addEventListener('click', clickButton);
+window.addEventListener('load', (event) => {
+    let alpha = document.getElementById('enter');
+    let bravo = document.getElementById('textarea')
+    let charlie = document.getElementById('Username')
+    let delta = document.getElementById('color-picker')
+    console.log(alpha, bravo,'the page has loaded')
+    alpha.addEventListener('click', () => {
+        console.log(`${charlie.value}(${delta.value}) says ${bravo.value}`)
+    })
+})
