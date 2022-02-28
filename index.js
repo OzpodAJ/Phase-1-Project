74deab9a9c40ac83f019d30e3722c47e55f7dc72
Andrew Osborn <osbornandrewj@gmail.com>
2022-02-28 15:35:26 -0800

    got the basic comment section working

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-// var post= document.getElementById("post");
-// post.addEventListener("click", function(){
-//     var commentBoxValue= document.getElementById("comment-box").value;
-//     var li = document.createElement("li");
-//     var text = document.createTextNode(commentBoxValue);
-//     li.appendChild(text);
-//     document.getElementById("unordered").appendChild(li);
- 
-// });
-    let post = document.getElementById('enter');
+let post = document.getElementById('enter');
     post.addEventListener('click', function(){
+        let userField = document.getElementById('username').value;
+        let color = document.getElementById('color-picker').value;
         let comment = document.getElementById('textarea').value;
+        let u = document.createElement('p')
         let p = document.createElement('p');
-        p.classList.add('message')
-        let text = document.createTextNode(comment)
+        u.classList.add(color);
+        p.classList.add('message');
+        let user = document.createTextNode(userField);
+        let text = document.createTextNode(comment);
+        u.appendChild(user)
         p.appendChild(text);
+        document.getElementById('messages').appendChild(u);
         document.getElementById('messages').appendChild(p);
     })
-    // let username = document.getElementById('Username').value;
-    // let colorValue = document.getElementById("color=picker").value;
-    // let name = doucment.createElement('p');
-    // name.classList.add(colorValue)
-    // let input = document.createTextNode(username);
-    // name.appendChild(input);
-    // doucment.getElementById('messages').appendChild(name);
+//To Do
+    //Assign color value to Username based on selector value
+//add reply box
+    //make hidden div
+    //add click event listener to clicking a comment
+        //check if a hidden div is already nested inside of that specific P
+        //if yes, simply reveal Div, if no, create new
+        //add button to re-hide div
+        //apply same username to div comments
+//make it all permanent affecting actual code/server instead of immediate DOM
