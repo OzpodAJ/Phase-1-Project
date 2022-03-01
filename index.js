100e0b7be5a5569c68bbb76e2f316698af2af16c
Andrew Osborn <osbornandrewj@gmail.com>
2022-03-01 12:29:52 -0800

    happy with css

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ let post = document.getElementById('enter');
         let comment = document.getElementById('textarea').value;
         let u = document.createElement('p')
         let p = document.createElement('div');
-        u.classList.add(color);
+        u.classList.add('userStyle');
+        u.style.color = color
         p.classList.add('p');
         p.setAttribute('id', 'message')
         let user = document.createTextNode(`${userField} says`);
@@ -25,21 +26,21 @@ let mBox = document.querySelector('#message')
         reply.setAttribute('type', 'submit')
         reply.setAttribute('id', 'replyButton')
         reply.textContent = 'reply'
-        // reply.onclick = () => {
-        //     let userField = document.getElementById('username').value;
-        //     let color = document.getElementById('color-picker').value;
-        //     let comment = document.getElementById('replyText').value;
-        //     let u = document.createElement('p')
-        //     let p = document.createElement('p');
-        //     u.classList.add(color, 'repUser');
-        //     p.classList.add('repCom')
-        //     let user = document.createTextNode(`${userField} says`);
-        //     let text = document.createTextNode(comment);
-        //     u.appendChild(user)
-        //     p.appendChild(text);
-        //     document.getElementById('replyDiv').appendChild(u);
-        //     document.getElementById('replyDiv').appendChild(p);
-        // };
+        reply.onclick = () => {
+            let userField = document.getElementById('username').value;
+            let color = document.getElementById('color-picker').value;
+            let comment = document.getElementById('replyText').value;
+            let u = document.createElement('p')
+            let p = document.createElement('p');
+            u.classList.add(color, 'repUser');
+            p.classList.add('repCom')
+            let user = document.createTextNode(`${userField} says`);
+            let text = document.createTextNode(comment);
+            u.appendChild(user)
+            p.appendChild(text);
+            document.getElementById('replyDiv').appendChild(u);
+            document.getElementById('replyDiv').appendChild(p);
+        };
         let hide = document.createElement('button');
         hide.setAttribute('id', 'hide');
         hide.setAttribute('type', 'submit');
@@ -57,18 +58,24 @@ let mBox = document.querySelector('#message')
             replyBox.style.display = "block"
         }
     })
-    reply.addEventListener('click', () => {
-        let userField = document.getElementById('username').value;
+    // reply.addEventListener('click', () => {
+    //     let userField = document.getElementById('username').value;
+    //     let color = document.getElementById('color-picker').value;
+    //     let comment = document.getElementById('replyText').value;
+    //     let u = document.createElement('p')
+    //     let p = document.createElement('p');
+    //     u.classList.add(color, 'repUser');
+    //     p.classList.add('repCom')
+    //     let user = document.createTextNode(`${userField} says`);
+    //     let text = document.createTextNode(comment);
+    //     u.appendChild(user)
+    //     p.appendChild(text);
+    //     document.getElementById('replyDiv').appendChild(u);
+    //     document.getElementById('replyDiv').appendChild(p);
+    // });
+    function setColor(){
         let color = document.getElementById('color-picker').value;
-        let comment = document.getElementById('replyText').value;
-        let u = document.createElement('p')
-        let p = document.createElement('p');
-        u.classList.add(color, 'repUser');
-        p.classList.add('repCom')
-        let user = document.createTextNode(`${userField} says`);
-        let text = document.createTextNode(comment);
-        u.appendChild(user)
-        p.appendChild(text);
-        document.getElementById('replyDiv').appendChild(u);
-        document.getElementById('replyDiv').appendChild(p);
-    });
+        picker.setAttribute('class', color)
+    }
+    let picker = document.getElementById('color-picker');
+    picker.addEventListener("mouseout",setColor);
