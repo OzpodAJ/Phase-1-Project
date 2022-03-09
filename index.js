c7388129bc5c5efdbd021c7df599e953b569a3c1
Andrew Osborn <osbornandrewj@gmail.com>
2022-03-09 10:01:28 -0800

    home stretch

--- index.js
+++ index.js
@@ -13,15 +13,30 @@ function postMessage(content){
     let newMessage = document.createElement('div')
     newMessage.className = 'post'
     newMessage.innerHTML = `
-    <p class="basic">${content.username}</p>
+    <p class="${content.color}">${content.username}</p>
     <p class="message">${content.message}</p>
+    <div class="replyDiv">
+        <form>
+            <input type="text" name="text" placeholder="your reply" id="replyText" required>
+            <button type="submit" id="rplyBtn" class="enter">Reply</button>
+        </form>
+    </div>
     `
     document.getElementById('messages').appendChild(newMessage)
-    // patchMessage(content)
 }
 function setColor(){
     let color = document.getElementById('colorpicker').value;
     picker.setAttribute('class', color)
+    console.log(picker.value)
+    if (picker.value === "red"){
+        picker.style.backgroundColor = "#f08080"
+    }else if (picker.value === "purple"){
+        picker.style.backgroundColor = "#CBC3E3"
+    }else if (picker.value === "green"){
+        picker.style.backgroundColor = "#93E9BE"
+    }else if (picker.value === "blue"){
+        picker.style.backgroundColor = "#89CFF0"
+    }
 }
 let picker = document.getElementById('colorpicker');
 picker.addEventListener("input",setColor);
@@ -31,7 +46,7 @@ function fetchMessage(){
     .then(res => res.json())
     .then(messageData => messageData.forEach(newMessage => postMessage(newMessage)))
 }
-
+fetchMessage()
 function pushMessage(postObj){
     fetch('http://localhost:3000/posts', {
         method: 'POST',
@@ -41,91 +56,5 @@ function pushMessage(postObj){
         body:JSON.stringify(postObj),
     })
     .then(res => res.json())
-    .then(newMessage => console.log(newMessage))
+    .then(newMessage => console.log(newMessage.id))
 }
-// function patchMessage(postObj){
-//     fetch(`http://localhost:3000/posts/${postObj.id}`, {
-//         method: 'PATCH',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body:JSON.stringify(postObj),
-//     })
-//     .then(res => res.json())
-//     .then(newMessage => console.log(newMessage))
-// }
-// let post = document.getElementById('enter');
-//     post.addEventListener('click', function(){
-//         let userField = document.getElementById('username').value;
-//         let color = document.getElementById('color-picker').value;
-//         let comment = document.getElementById('textarea').value;
-//         let u = document.createElement('p')
-//         let p = document.createElement('div');
-//         u.classList.add('userStyle');
-//         u.style.color = color
-//         p.classList.add('p');
-//         p.setAttribute('id', 'message')
-//         let user = document.createTextNode(`${userField} says`);
-//         let text = document.createTextNode(comment);
-//         u.appendChild(user)
-//         p.appendChild(text);
-//         document.getElementById('messages').appendChild(u);
-//         document.getElementById('messages').appendChild(p);
-//     })
-// let mBox = document.querySelector('#message')
-//     mBox.addEventListener('click', () => {
-//         let replyText = document.createElement('input')
-//         replyText.setAttribute('id', 'replyText')
-//         replyText.setAttribute('type', 'text')
-//         replyText.setAttribute('placeholder', 'Type your reply here')
-//         let reply = document.createElement('button')
-//         reply.setAttribute('type', 'submit')
-//         reply.setAttribute('id', 'replyButton')
-//         reply.textContent = 'reply'
-//         reply.onclick = () => {
-//             let userField = document.getElementById('username').value;
-//             let color = document.getElementById('color-picker').value;
-//             let comment = document.getElementById('replyText').value;
-//             let u = document.createElement('p')
-//             let p = document.createElement('p');
-//             u.classList.add(color, 'repUser');
-//             p.classList.add('repCom')
-//             let user = document.createTextNode(`${userField} says`);
-//             let text = document.createTextNode(comment);
-//             u.appendChild(user)
-//             p.appendChild(text);
-//             document.getElementById('replyDiv').appendChild(u);
-//             document.getElementById('replyDiv').appendChild(p);
-//         };
-//         let hide = document.createElement('button');
-//         hide.setAttribute('id', 'hide');
-//         hide.setAttribute('type', 'submit');
-//         hide.textContent = 'Hide Comments';
-//         hide.onclick = () => {replyBox.style.display = "none";};
-//         let replyBox = document.createElement('div');
-//         replyBox.setAttribute('id', 'replyDiv');
-//         replyBox.appendChild(replyText);
-//         replyBox.appendChild(reply);
-//         replyBox.appendChild(hide);
-//         if (document.getElementById('replyDiv') == null) {
-//             mBox.appendChild(replyBox);
-//             replyBox.style.display = "block"
-//         } else if (document.getElementById('replyDiv') != null){
-//             replyBox.style.display = "block"
-//         }
-//     })
-    // reply.addEventListener('click', () => {
-    //     let userField = document.getElementById('username').value;
-    //     let color = document.getElementById('color-picker').value;
-    //     let comment = document.getElementById('replyText').value;
-    //     let u = document.createElement('p')
-    //     let p = document.createElement('p');
-    //     u.classList.add(color, 'repUser');
-    //     p.classList.add('repCom')
-    //     let user = document.createTextNode(`${userField} says`);
-    //     let text = document.createTextNode(comment);
-    //     u.appendChild(user)
-    //     p.appendChild(text);
-    //     document.getElementById('replyDiv').appendChild(u);
-    //     document.getElementById('replyDiv').appendChild(p);
-    // });
